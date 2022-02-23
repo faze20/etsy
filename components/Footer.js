0d24ab4e0c483fd4845e2344b705b746c6fe4704
faze20 <42577269+faze20@users.noreply.github.com>
2022-02-23 07:05:40 -0800

    footer1

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Link  from 'next/link'
-import { FiFacebook } from "react-icons/fa";
+import { FiFacebook ,FiTwitter , FiInstagram} from "react-icons/fi";
+import { HiOutlineAtSymbol } from "react-icons/hi";
+import { FaPinterestP } from "react-icons/fa";
+
 
 
 function Footer() {
   return (
     <div className='grid  grid-cols-1 py-4 mx-3 mt-12 md:grid-cols-4 gap-4'>
 {/* first div footer start */}
-      <div>
-        <h2 className='text-4xl tracking-tighter  font-bold'>
+      <div className='my-6 text-center md:text-left'>
+        <h2 className='text-2xl tracking-tighter  font-bold'>
           SHOP
         </h2>
         <div>
@@ -43,43 +46,47 @@ function Footer() {
 {/* first div footer end*/}
 
 {/* second div footer start */}
-<div>
-        <h2 className='text-4xl tracking-tighter  font-bold'>
-          FOLLOW US OUT THERE
+<div className='my-6 text-center  md:text-left'>
+        <h2 className='text-2xl tracking-tighter  font-bold'>
+          FOLLOW US ON SOCIAL
         </h2>
         <div>
-          <ul className='space-y-4 mt-4'>
-            <li>
+          <ul className='space-y-4 mt-4 flex flex-col items-center '>
+            <li className='flex items-center'>
+              <FiFacebook />
+              <Link  href="https://facebook.com.com/" passHref={true} >
+                  <a className='mx-2'> Facebook</a> 
+              </Link>
+            </li>
 
-          <Link  href="/" >
-              <a> Facebook</a> 
-          </Link>
-              {/* <FiFacebook /> */}
+            <li className='flex items-center'>
+              <FiTwitter />
+              <Link  href="https://twitter.com/" passHref={true} >
+                  <a className='mx-2'>Twitter</a> 
+              </Link>
             </li>
-            <li>
-              
-          <Link  href="shop" >
-              <a>Twitter</a> 
-          </Link>
+
+            <li className='flex items-center'>
+              <FaPinterestP />
+              <Link  href="https://pinterest.com/" passHref={true} >
+                  <a className='mx-2'>Pinterest</a> 
+              </Link>
             </li>
-            <li>
-              
-          <Link  href="policy" >
-              <a>Pinterest</a> 
-          </Link>
+
+            <li className='flex items-center'>
+              <FiInstagram />
+              <Link  href="https://instagram.com/" passHref={true} >
+                  <a className='mx-2'>Instagram</a> 
+              </Link>
             </li>
-            <li>
-              
-          <Link  href="contact" >
-              <a>Instagram</a> 
-          </Link>
+
+            <li className='flex items-center'>
+              <HiOutlineAtSymbol />
+              <Link  href="contact" >
+                  <a className='mx-2'>email</a> 
+              </Link>
             </li>
-            <li>
-              
-          <Link  href="contact" >
-              <a>email</a> 
-          </Link>
-            </li>
+
           </ul>
         </div>
       </div>
@@ -87,8 +94,8 @@ function Footer() {
 {/* second div footer end */}
 
 {/* third div footer start */}
-<div>
-<h2 className='text-4xl tracking-tighter  font-bold'>
+<div className='my-6 text-center  md:text-left'>
+<h2 className='text-2xl tracking-tighter  font-bold'>
     Sign Up For Emails
   </h2>
   <p className='mt-4'>
@@ -100,14 +107,17 @@ function Footer() {
 {/* third div footer end */}
 
 {/* fourth div footer start */}
-<div>
+<div className='my-6 text-center  md:text-left'>
 
-<h2 className='text-4xl tracking-tighter  font-bold'>
+<h2 className='text-2xl tracking-tighter  font-bold'>
     NEWSLETTER
   </h2>
   <form className='mt-4'>
-    <input type="email" placeholder='your@email.com'/>
-    <input type="submit" value="SUBSCRIBE" />
+    <input className='border-solid border border-gray-200 p-1 outline-0' type="email" placeholder='your@email.com'/>
+    <button className='px-4 py-1 bg-black text-white'>
+        Subscribe
+    </button>
+    {/* <input type="submit" value="SUBSCRIBE" /> */}
   </form>
 </div>
 
